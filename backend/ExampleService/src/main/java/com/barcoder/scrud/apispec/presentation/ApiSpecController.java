9958d3fc4f8f4b0cddbcad29b0f196cadeca5ff5
KwonSeon <ksun2128@pukyong.ac.kr>
2025-05-06 20:46:00 +0900

    Refactor: api spec version request and response 수정

--- backend/ExampleService/src/main/java/com/barcoder/scrud/apispec/presentation/ApiSpecController.java
+++ backend/ExampleService/src/main/java/com/barcoder/scrud/apispec/presentation/ApiSpecController.java
@@ -5,9 +5,9 @@ import com.barcoder.scrud.apispec.application.dto.in.CreateApiSpecVersionIn;
 import com.barcoder.scrud.apispec.application.dto.out.ApiSpecVersionOut;
 import com.barcoder.scrud.apispec.application.facade.ApiCreateFacade;
 import com.barcoder.scrud.apispec.application.service.ApiSpecVersionService;
-import com.barcoder.scrud.model.ApiSpecVersionRequest;
+import com.barcoder.scrud.model.ApiSpecVersionCreateRequest;
+import com.barcoder.scrud.model.ApiSpecVersionCreatedResponse;
 import com.barcoder.scrud.model.ApiSpecWithFields;
-import com.barcoder.scrud.model.CreateApiSpec201Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -24,23 +24,45 @@ public class ApiSpecController implements ApiSpecApi {
 	private final ApiCreateFacade apiCreateFacade;
 	private final ModelMapper modelMapper;
 
+	/**
+	 * POST /api/v1/api-specs : API 스펙 생성
+	 * 새로운 API 스펙과 필드를 등록합니다.
+	 *
+	 * @param request 단일 API 스펙과 필드를 전체 생성합니다. (required)
+	 * @return ApiSpecVersionCreatedResponse API 스펙 생성 성공 (status code 201)
+	 */
 	@Override
-	public ResponseEntity<CreateApiSpec201Response> createApiSpec(ApiSpecVersionRequest apiSpecRequest) {
+	public ResponseEntity<ApiSpecVersionCreatedResponse> createApiSpec(ApiSpecVersionCreateRequest request) {
 
-		CreateApiSpecVersionIn inDto = modelMapper.map(apiSpecRequest, CreateApiSpecVersionIn.class);
+		CreateApiSpecVersionIn inDto = modelMapper.map(request, CreateApiSpecVersionIn.class);
 		ApiSpecVersionOut outDto = apiCreateFacade.createApiSpecVersion(inDto);
-		CreateApiSpec201Response response = modelMapper.map(outDto, CreateApiSpec201Response.class);
+		ApiSpecVersionCreatedResponse response = modelMapper.map(outDto, ApiSpecVersionCreatedResponse.class);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(response);
 	}
 
+	/**
+	 * GET /api/v1/api-specs/{apiSpecVersionId} : API 스펙 상세 조회
+	 * 단일 API 스펙과 관련 필드를 조회합니다.
+	 *
+	 * @param apiSpecVersionId (required)
+	 * @return ApiSpecWithFields 단일 API 스펙 조회 성공 (status code 200)
+	 */
 	@Override
 	public ResponseEntity<ApiSpecWithFields> getApiSpecById(Integer apiSpecVersionId) {
 		return null;
 	}
 
+	/**
+	 * PUT /api/v1/api-specs/{apiSpecVersionId} : API 스펙 수정
+	 * 단일 API 스펙과 필드를 전체 수정합니다.
+	 *
+	 * @param apiSpecVersionId            (required)
+	 * @param apiSpecVersionCreateRequest 단일 API 스펙과 필드를 전체 생성합니다. (required)
+	 * @return Boolean API 스펙 수정 성공 (status code 200)
+	 */
 	@Override
-	public ResponseEntity<Boolean> updateApiSpec(Integer apiSpecVersionId, ApiSpecVersionRequest apiSpecRequest) {
+	public ResponseEntity<Boolean> updateApiSpec(Integer apiSpecVersionId, ApiSpecVersionCreateRequest apiSpecVersionCreateRequest) {
 		return null;
 	}
 }
