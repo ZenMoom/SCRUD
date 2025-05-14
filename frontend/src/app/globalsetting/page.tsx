31216e3c6f9a92c9f1f6adbbbd2d1b899e5df4b1
조소현 <vjawb2262@gmail.com>
2025-05-14 10:48:42 +0900

    Design:아키텍처 구조도 이미지 추가+카드 레이아웃 변경

--- frontend/src/app/globalsetting/page.tsx
+++ frontend/src/app/globalsetting/page.tsx
@@ -269,23 +269,6 @@ export default function GlobalSettingPage() {
         <TokenHandler />
       </Suspense>
       
-      <h1 className="text-2xl font-bold mb-6">SCRUD</h1>
-      <div className="mb-6">
-        <div className="flex items-center mb-4">
-          <div className="w-1/3 text-center relative">
-            <div className="w-full absolute top-1/2 h-1 bg-gray-300 -z-10"></div>
-            <span className="bg-blue-500 text-white px-4 py-2 rounded-full">전역 설정</span>
-          </div>
-          <div className="w-1/3 text-center relative">
-            <div className="w-full absolute top-1/2 h-1 bg-gray-300 -z-10"></div>
-            <span className="bg-gray-200 text-gray-700 px-4 py-2 rounded-full">API 제작</span>
-          </div>
-          <div className="w-1/3 text-center relative">
-            <div className="absolute top-1/2 h-1 bg-gray-300 -z-10 w-full"></div>
-            <span className="bg-gray-200 text-gray-700 px-4 py-2 rounded-full">API 도식화</span>
-          </div>
-        </div>
-      </div>
       <div className="flex flex-col h-[90vh] w-full bg-[#fafafa]">
         <div className="flex flex-1 overflow-hidden">
           <Sidebar completed={completed} activeItem={activeItem} onItemClick={handleItemClick} />
