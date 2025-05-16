48ee0cd440aefcf98d37013916e00e163eb2474a
권선 <ksun2128@pukyong.ac.kr>
2025-05-16 11:30:10 +0900

    Feat: 게시글 목록 조회 기능 추가
    게시글 목록 조회 기능 구현을 위해 DTO, QueryDSL, Enum, Controller, Service 계층에 관련 로직 및 객체 추가

--- backend/ExampleService/src/main/java/com/barcoder/scrud/post/application/service/PostService.java
+++ backend/ExampleService/src/main/java/com/barcoder/scrud/post/application/service/PostService.java
@@ -3,12 +3,17 @@ package com.barcoder.scrud.post.application.service;
 import com.barcoder.scrud.global.common.exception.BaseException;
 import com.barcoder.scrud.post.application.assembler.PostAssembler;
 import com.barcoder.scrud.post.application.dto.in.CreatePostIn;
+import com.barcoder.scrud.post.application.dto.in.GetPostListIn;
 import com.barcoder.scrud.post.application.dto.out.CreatePostOut;
+import com.barcoder.scrud.post.application.dto.out.PostListOut;
 import com.barcoder.scrud.post.domain.entity.Category;
 import com.barcoder.scrud.post.domain.entity.Post;
 import com.barcoder.scrud.post.domain.exception.PostErrorStatus;
+import com.barcoder.scrud.post.domain.query.in.PostListQueryIn;
+import com.barcoder.scrud.post.domain.query.out.PostListQueryOut;
 import com.barcoder.scrud.post.infrastructure.jpa.CategoryJpaRepository;
 import com.barcoder.scrud.post.infrastructure.jpa.PostJpaRepository;
+import com.barcoder.scrud.post.infrastructure.querydsl.PostQueryDsl;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -22,8 +27,15 @@ public class PostService {
     private final PostAssembler postAssembler;
     private final CategoryJpaRepository categoryJpaRepository;
     private final PostJpaRepository postJpaRepository;
+    private final PostQueryDsl postQueryDsl;
     private final ModelMapper modelMapper;
 
+    /**
+     * 게시글 생성
+     *
+     * @param inDto 게시글 생성 요청 DTO
+     * @return CreatePostOut 게시글 생성 응답 DTO
+     */
     public CreatePostOut createPost(CreatePostIn inDto) {
 
         // category 조회
@@ -38,5 +50,21 @@ public class PostService {
 
         // outDto 생성 및 반환
         return modelMapper.map(post, CreatePostOut.class);
+    }
+
+    @Transactional(readOnly = true)
+    public PostListOut getPostList(GetPostListIn inDto) {
+
+        // queryInDto 생성
+        PostListQueryIn queryIn = modelMapper.map(inDto, PostListQueryIn.class);
+
+        // 리스트 조회
+        PostListQueryOut queryOut = postQueryDsl.getPostList(queryIn);
+
+        // outDto 생성
+        PostListOut outDto = modelMapper.map(queryOut, PostListOut.class);
+
+        // 반환
+        return outDto;
     }
 }
--- backend/ExampleService/src/main/java/com/barcoder/scrud/post/presentation/PostController.java
+++ backend/ExampleService/src/main/java/com/barcoder/scrud/post/presentation/PostController.java
@@ -5,22 +5,30 @@ import com.barcoder.scrud.global.common.util.SecurityUtil;
 import com.barcoder.scrud.model.CreatePostRequest;
 import com.barcoder.scrud.model.CreatePostResponse;
 import com.barcoder.scrud.model.GetPostListResponse;
+import com.barcoder.scrud.model.PageDto;
 import com.barcoder.scrud.model.PostDetailResponse;
 import com.barcoder.scrud.model.PostOrderEnumDto;
 import com.barcoder.scrud.model.PostSortEnumDto;
+import com.barcoder.scrud.model.PostSummaryResponse;
 import com.barcoder.scrud.model.PostVoteRequest;
 import com.barcoder.scrud.model.SearchTypeEnumDto;
 import com.barcoder.scrud.model.UpdatePostRequest;
 import com.barcoder.scrud.model.VoteResponse;
 import com.barcoder.scrud.post.application.dto.in.CreatePostIn;
+import com.barcoder.scrud.post.application.dto.in.GetPostListIn;
 import com.barcoder.scrud.post.application.dto.out.CreatePostOut;
+import com.barcoder.scrud.post.application.dto.out.PostListOut;
 import com.barcoder.scrud.post.application.service.PostService;
+import com.barcoder.scrud.post.domain.enums.PostOrder;
+import com.barcoder.scrud.post.domain.enums.PostSearchType;
+import com.barcoder.scrud.post.domain.enums.PostSort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -93,8 +101,41 @@ public class PostController implements PostApi {
      * @return GetPostListResponse 게시글 목록 조회 응답 (status code 200)
      */
     @Override
-    public ResponseEntity<GetPostListResponse> getPostList(Integer page, Integer size, PostSortEnumDto sort, PostOrderEnumDto order, String keyword, SearchTypeEnumDto type, Integer categoryId) {
-        return null;
+    public ResponseEntity<GetPostListResponse> getPostList(Integer page, Integer size, PostSortEnumDto sort, PostOrderEnumDto order, String keyword, SearchTypeEnumDto type, Long categoryId) {
+
+        // inDto 생성
+        GetPostListIn inDto = GetPostListIn.builder()
+                .page(page)
+                .size(size)
+                .sort(modelMapper.map(sort, PostSort.class))
+                .order(modelMapper.map(order, PostOrder.class))
+                .keyword(keyword)
+                .type(modelMapper.map(type, PostSearchType.class))
+                .categoryId(categoryId)
+                .build();
+
+        // 게시글 목록 조회
+        PostListOut outDto = postService.getPostList(inDto);
+
+        // pageDto 변환
+        PageDto pageDto = modelMapper.map(outDto, PageDto.class);
+
+        // content 변환
+        List<PostSummaryResponse> postResponseList = outDto.getContent().stream()
+                .map(post -> {
+                    PostSummaryResponse postSummaryResponse = modelMapper.map(post, PostSummaryResponse.class);
+                    postSummaryResponse.setCategory(post.getCategory().getName());
+                    return postSummaryResponse;
+                })
+                .toList();
+
+        //response 변환
+        GetPostListResponse response = GetPostListResponse.builder()
+                .pageable(pageDto)
+                .content(postResponseList)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
     /**
