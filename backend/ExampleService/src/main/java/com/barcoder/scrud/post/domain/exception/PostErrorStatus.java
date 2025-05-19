9b1963ef3a5dd26d6c1b78740a330cb17f675beb
KwonSeon <ksun2128@pukyong.ac.kr>
2025-05-20 03:56:19 +0900

    Feat: 본인 게시글 추천/비추천 방지 로직 추가 및 관련 에러 상태 코드 추가

--- backend/ExampleService/src/main/java/com/barcoder/scrud/post/domain/exception/PostErrorStatus.java
+++ backend/ExampleService/src/main/java/com/barcoder/scrud/post/domain/exception/PostErrorStatus.java
@@ -16,7 +16,8 @@ public enum PostErrorStatus implements BaseErrorCode {
 	// post error
 	POST_NOT_FOUND(HttpStatus.NOT_FOUND, "POST5101", "존재하지 않는 게시글입니다."),
 	POST_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "POST5102", "게시글 작성자가 아닙니다."),
-	POST_ALREADY_VOTED(HttpStatus.BAD_REQUEST, "POST5103", "이미 추천한 게시글입니다."),
+	POST_VOTE_SELF(HttpStatus.BAD_REQUEST, "POST5103", "자신의 게시글에 추천/비추천을 할 수 없습니다."),
+	POST_ALREADY_VOTED(HttpStatus.BAD_REQUEST, "POST5104", "이미 추천한 게시글입니다."),
 
 	// comment error
 	COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "POST5201", "존재하지 않는 댓글입니다."),
@@ -25,7 +26,7 @@ public enum PostErrorStatus implements BaseErrorCode {
 	COMMENT_ALREADY_VOTED(HttpStatus.BAD_REQUEST, "POST5204", "이미 추천/비추천한 댓글입니다."),
 
 
-;
+	;
 	private final HttpStatus httpStatus;
 	private final String code;
 	private final String message;
