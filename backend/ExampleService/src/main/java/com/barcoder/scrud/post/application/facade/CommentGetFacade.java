4b90b8eb8c8c6ae0c26dd3767d714b0070f677cd
권선 <ksun2128@pukyong.ac.kr>
2025-05-21 17:44:01 +0900

    Refactor: CommentController 및 CommentGetFacade 들여쓰기 및 코드 스타일 정리
    - 전체적으로 코드의 들여쓰기 수정 및 일관성 있는 포맷으로 정리
    - 가독성을 높이기 위해 주석 및 변수 선언 위치 정리

--- backend/ExampleService/src/main/java/com/barcoder/scrud/post/application/facade/CommentGetFacade.java
+++ backend/ExampleService/src/main/java/com/barcoder/scrud/post/application/facade/CommentGetFacade.java
@@ -28,94 +28,84 @@ import java.util.stream.Collectors;
 @Slf4j
 public class CommentGetFacade {
 
-	private final PostJpaRepository postJpaRepository;
-	private final CommentVoteJpaRepository commentVoteJpaRepository;
-	private final UserUseCase userUseCase;
-	private final ModelMapper modelMapper;
+    private final PostJpaRepository postJpaRepository;
+    private final CommentVoteJpaRepository commentVoteJpaRepository;
+    private final UserUseCase userUseCase;
+    private final ModelMapper modelMapper;
 
-	public List<CommentOut> getCommentList(Long postId, UUID userId) {
+    public List<CommentOut> getCommentList(Long postId, UUID userId) {
 
-		// 게시글 조회
-		Post post = postJpaRepository.findById(postId)
-				.orElseThrow(() -> new ExceptionHandler(PostErrorStatus.POST_NOT_FOUND));
+        // 게시글 조회
+        Post post = postJpaRepository.findById(postId)
+                .orElseThrow(() -> new ExceptionHandler(PostErrorStatus.POST_NOT_FOUND));
 
-		// 댓글 리스트 조회
-		List<Comment> allComents = post.getComments();
+        // 댓글 리스트 조회
+        List<Comment> allComents = post.getComments();
 
-		// 댓글이 없는 경우
-		if (allComents.isEmpty()) {
-			return List.of();
-		}
+        // 댓글이 없는 경우
+        if (allComents.isEmpty()) {
+            return List.of();
+        }
 
-		// userId 리스트 생성
-		Set<UUID> userIdList = allComents.stream()
-				.map(Comment::getUserId)
-				.collect(Collectors.toSet());
+        // userId 리스트 생성
+        Set<UUID> userIdList = allComents.stream()
+                .map(Comment::getUserId)
+                .collect(Collectors.toSet());
 
-		// userOut 조회
-		for (UUID uuid : userIdList) {
-			userUseCase.getUserById(uuid);
-		}
+        // userOut 조회
+        for (UUID uuid : userIdList) {
+            userUseCase.getUserById(uuid);
+        }
 
-		// commentIds 리스트 추출 추가 필요
-		List<Long> commentIds = allComents.stream()
-				.map(Comment::getCommentId)
-				.toList();
-		for (Long commentId : commentIds) {
-			log.info("Comment Id: {}", commentId);
-		}
-
-		List<CommentVote> allById = commentVoteJpaRepository.findAllByComment_CommentIdIn(commentIds);
-		for (CommentVote commentVote : allById) {
-			log.info("Comment Vote Id: {}, User Id: {}, Is Like: {}", commentVote.getComment().getCommentId(), commentVote.getUserId(), commentVote.getIsLike());
-		}
-		log.info("userId: {}", userId);
-
-		Map<Long, String> commentIdToUserVoteMap = (userId != null)
-				? commentVoteJpaRepository
-				.findByUserIdAndComment_CommentIdIn(userId, commentIds)
-				.stream()
-				.collect(Collectors.toMap(
-						vote -> vote.getComment().getCommentId(),
-						vote -> vote.getIsLike() ? "LIKE" : "DISLIKE"
-				))
-				: Map.of();
-
-		for (Long l : commentIdToUserVoteMap.keySet()) {
-			log.info("Comment Id: {}, User Vote: {}", l, commentIdToUserVoteMap.get(l));
-		}
+        // commentIds 리스트 추출 추가 필요
+        List<Long> commentIds = allComents.stream()
+                .map(Comment::getCommentId)
+                .toList();
 
 
-		// commentOut 리스트 생성
-		List<CommentOut> content = allComents.stream()
-				.map(comment -> {
+        List<CommentVote> allById = commentVoteJpaRepository.findAllByComment_CommentIdIn(commentIds);
 
-					// userOut 조회
-					UserOut userOut = userUseCase.getUserById(comment.getUserId());
 
-					// parentCommentId가 null인 경우
-					Long parentCommentId = comment.getParentComment() != null
-							? comment.getParentComment().getCommentId()
-							: null;
+        Map<Long, String> commentIdToUserVoteMap = (userId != null)
+                ? commentVoteJpaRepository
+                .findByUserIdAndComment_CommentIdIn(userId, commentIds)
+                .stream()
+                .collect(Collectors.toMap(
+                        vote -> vote.getComment().getCommentId(),
+                        vote -> vote.getIsLike() ? "LIKE" : "DISLIKE"
+                ))
+                : Map.of();
 
-					// userVote 조회
-					String userVote = "NONE";
-					if (userId != null) {
-						userVote = commentIdToUserVoteMap.getOrDefault(comment.getCommentId(), "NONE");
-					}
+        // commentOut 리스트 생성
+        List<CommentOut> content = allComents.stream()
+                .map(comment -> {
 
-					// commentOut 생성
-					return modelMapper.map(comment, CommentOut.class).toBuilder()
-							.postId(comment.getPost().getPostId())
-							.parentCommentId(parentCommentId)
-							.userVote(userVote)
-							.author(userOut)
+                    // userOut 조회
+                    UserOut userOut = userUseCase.getUserById(comment.getUserId());
 
-							.build();
-				})
-				.toList();
+                    // parentCommentId가 null인 경우
+                    Long parentCommentId = comment.getParentComment() != null
+                            ? comment.getParentComment().getCommentId()
+                            : null;
 
-		// outDto 생성
-		return content;
-	}
+                    // userVote 조회
+                    String userVote = "NONE";
+                    if (userId != null) {
+                        userVote = commentIdToUserVoteMap.getOrDefault(comment.getCommentId(), "NONE");
+                    }
+
+                    // commentOut 생성
+                    return modelMapper.map(comment, CommentOut.class).toBuilder()
+                            .postId(comment.getPost().getPostId())
+                            .parentCommentId(parentCommentId)
+                            .userVote(userVote)
+                            .author(userOut)
+
+                            .build();
+                })
+                .toList();
+
+        // outDto 생성
+        return content;
+    }
 }
--- backend/ExampleService/src/main/java/com/barcoder/scrud/post/presentation/CommentController.java
+++ backend/ExampleService/src/main/java/com/barcoder/scrud/post/presentation/CommentController.java
@@ -31,147 +31,147 @@ import java.util.UUID;
 @Slf4j
 public class CommentController implements CommentApi {
 
-	private final CommentFacade commentFacade;
-	private final CommentGetFacade commentGetFacade;
-	private final SecurityUtil securityUtil;
-	private final ModelMapper modelMapper;
+    private final CommentFacade commentFacade;
+    private final CommentGetFacade commentGetFacade;
+    private final SecurityUtil securityUtil;
+    private final ModelMapper modelMapper;
 
 
-	/**
-	 * POST /api/v1/posts/{postId}/comments : 댓글 작성
-	 *
-	 * @param postId               (required)
-	 * @param createCommentRequest 댓글 작성 요청 (required)
-	 * @return Void 성공적으로 생성되었습니다 (status code 201)
-	 */
-	@Override
-	public ResponseEntity<CommentResponse> createComment(Long postId, CreateCommentRequest createCommentRequest) {
+    /**
+     * POST /api/v1/posts/{postId}/comments : 댓글 작성
+     *
+     * @param postId               (required)
+     * @param createCommentRequest 댓글 작성 요청 (required)
+     * @return Void 성공적으로 생성되었습니다 (status code 201)
+     */
+    @Override
+    public ResponseEntity<CommentResponse> createComment(Long postId, CreateCommentRequest createCommentRequest) {
 
-		// userId 조회
-		UUID userId = securityUtil.getCurrentUserId();
+        // userId 조회
+        UUID userId = securityUtil.getCurrentUserId();
 
-		// inDto 생성
-		CreateCommentIn inDto = modelMapper.map(createCommentRequest, CreateCommentIn.class).toBuilder()
-				.postId(postId)
-				.userId(userId)
-				.build();
+        // inDto 생성
+        CreateCommentIn inDto = modelMapper.map(createCommentRequest, CreateCommentIn.class).toBuilder()
+                .postId(postId)
+                .userId(userId)
+                .build();
 
-		// 댓글 작성
-		CommentOut outDto = commentFacade.createComment(inDto);
+        // 댓글 작성
+        CommentOut outDto = commentFacade.createComment(inDto);
 
-		// response 변환
-		CommentResponse response = modelMapper.map(outDto, CommentResponse.class);
+        // response 변환
+        CommentResponse response = modelMapper.map(outDto, CommentResponse.class);
 
-		// 반환
-		return ResponseEntity.ok(response);
-	}
+        // 반환
+        return ResponseEntity.ok(response);
+    }
 
-	/**
-	 * DELETE /api/v1/comments/{commentId} : 댓글 삭제
-	 *
-	 * @param commentId (required)
-	 * @return Void 성공적으로 처리되었습니다 (status code 204)
-	 */
-	@Override
-	public ResponseEntity<Void> deleteComment(Long commentId) {
-		// userId 조회
-		UUID userId = securityUtil.getCurrentUserId();
-		// 댓글 삭제
-		commentFacade.deleteComment(commentId, userId);
-		return ResponseEntity.ok().build();
-	}
+    /**
+     * DELETE /api/v1/comments/{commentId} : 댓글 삭제
+     *
+     * @param commentId (required)
+     * @return Void 성공적으로 처리되었습니다 (status code 204)
+     */
+    @Override
+    public ResponseEntity<Void> deleteComment(Long commentId) {
+        // userId 조회
+        UUID userId = securityUtil.getCurrentUserId();
+        // 댓글 삭제
+        commentFacade.deleteComment(commentId, userId);
+        return ResponseEntity.ok().build();
+    }
 
-	/**
-	 * GET /api/v1/posts/{postId}/comments : 댓글 조회
-	 *
-	 * @param postId (required)
-	 * @return GetCommentListResponse 댓글 목록 조회 성공 (status code 200)
-	 */
-	@Override
-	public ResponseEntity<GetCommentListResponse> getCommentList(Long postId) {
+    /**
+     * GET /api/v1/posts/{postId}/comments : 댓글 조회
+     *
+     * @param postId (required)
+     * @return GetCommentListResponse 댓글 목록 조회 성공 (status code 200)
+     */
+    @Override
+    public ResponseEntity<GetCommentListResponse> getCommentList(Long postId) {
 
-		// userId 조회
-		UUID userId = null;
+        // userId 조회
+        UUID userId = null;
 
-		try {
-			userId = securityUtil.getCurrentUserId(); // 로그인 사용자면 userId 반환
-		} catch (ExceptionHandler e) {
-			if (e.getCode() == ErrorStatus._UNAUTHORIZED) {
-				log.info("비로그인 사용자 → 댓글 투표 상태 없음");
-			} else {
-				throw e; // 그 외는 예외 터뜨림
-			}
-		}
+        try {
+            userId = securityUtil.getCurrentUserId(); // 로그인 사용자면 userId 반환
+        } catch (ExceptionHandler e) {
+            if (e.getCode() == ErrorStatus._UNAUTHORIZED) {
+                log.info("비로그인 사용자 → 댓글 투표 상태 없음");
+            } else {
+                throw e; // 그 외는 예외 터뜨림
+            }
+        }
 
-		// 댓글 목록 조회
-		List<CommentOut> outDtoList = commentGetFacade.getCommentList(postId, userId);
+        // 댓글 목록 조회
+        List<CommentOut> outDtoList = commentGetFacade.getCommentList(postId, userId);
 
-		// response 변환
-		List<CommentResponse> content = outDtoList.stream()
-				.map(commentOut -> {
-					CommentResponse comment = modelMapper.map(commentOut, CommentResponse.class);
-					if (comment.getIsDeleted()) {
-						comment.setContent("삭제된 댓글입니다.");
-					}
-					return comment;
-				})
-				.toList();
+        // response 변환
+        List<CommentResponse> content = outDtoList.stream()
+                .map(commentOut -> {
+                    CommentResponse comment = modelMapper.map(commentOut, CommentResponse.class);
+                    if (comment.getIsDeleted()) {
+                        comment.setContent("삭제된 댓글입니다.");
+                    }
+                    return comment;
+                })
+                .toList();
 
-		GetCommentListResponse response = GetCommentListResponse.builder()
-				.content(content)
-				.build();
+        GetCommentListResponse response = GetCommentListResponse.builder()
+                .content(content)
+                .build();
 
-		return ResponseEntity.ok(response);
-	}
+        return ResponseEntity.ok(response);
+    }
 
-	/**
-	 * PATCH /api/v1/comments/{commentId} : 댓글 수정
-	 *
-	 * @param commentId            (required)
-	 * @param updateCommentRequest 댓글을 수정합니다. (required)
-	 * @return Void 성공적으로 처리되었습니다 (status code 200)
-	 */
-	@Override
-	public ResponseEntity<Void> updateComment(Long commentId, UpdateCommentRequest updateCommentRequest) {
+    /**
+     * PATCH /api/v1/comments/{commentId} : 댓글 수정
+     *
+     * @param commentId            (required)
+     * @param updateCommentRequest 댓글을 수정합니다. (required)
+     * @return Void 성공적으로 처리되었습니다 (status code 200)
+     */
+    @Override
+    public ResponseEntity<Void> updateComment(Long commentId, UpdateCommentRequest updateCommentRequest) {
 
-		// userId 조회
-		UUID userId = securityUtil.getCurrentUserId();
+        // userId 조회
+        UUID userId = securityUtil.getCurrentUserId();
 
-		// inDto 생성
-		UpdateCommentIn inDto = modelMapper.map(updateCommentRequest, UpdateCommentIn.class).toBuilder()
-				.commentId(commentId)
-				.userId(userId)
-				.build();
+        // inDto 생성
+        UpdateCommentIn inDto = modelMapper.map(updateCommentRequest, UpdateCommentIn.class).toBuilder()
+                .commentId(commentId)
+                .userId(userId)
+                .build();
 
-		// 댓글 수정
-		commentFacade.updateComment(inDto);
+        // 댓글 수정
+        commentFacade.updateComment(inDto);
 
-		return ResponseEntity.ok().build();
-	}
+        return ResponseEntity.ok().build();
+    }
 
-	/**
-	 * POST /api/v1/comments/{commentId}/vote : 댓글 추천/비추천
-	 *
-	 * @param commentId          (required)
-	 * @param commentVoteRequest 댓글 추천/비추천 요청입니다. (required)
-	 * @return VoteCommentResponse 댓글 추천/비추천 성공 (status code 200)
-	 */
-	@Override
-	public ResponseEntity<VoteCommentResponse> voteComment(Long commentId, CommentVoteRequest commentVoteRequest) {
-		// userId 조회
-		UUID userId = securityUtil.getCurrentUserId();
+    /**
+     * POST /api/v1/comments/{commentId}/vote : 댓글 추천/비추천
+     *
+     * @param commentId          (required)
+     * @param commentVoteRequest 댓글 추천/비추천 요청입니다. (required)
+     * @return VoteCommentResponse 댓글 추천/비추천 성공 (status code 200)
+     */
+    @Override
+    public ResponseEntity<VoteCommentResponse> voteComment(Long commentId, CommentVoteRequest commentVoteRequest) {
+        // userId 조회
+        UUID userId = securityUtil.getCurrentUserId();
 
-		// inDto 생성
-		CommentVoteIn inDto = modelMapper.map(commentVoteRequest, CommentVoteIn.class).toBuilder()
-				.userId(userId)
-				.commentId(commentId)
-				.build();
+        // inDto 생성
+        CommentVoteIn inDto = modelMapper.map(commentVoteRequest, CommentVoteIn.class).toBuilder()
+                .userId(userId)
+                .commentId(commentId)
+                .build();
 
-		// 댓글 추천/비추천
-		CommentVoteOut outDto = commentFacade.voteComment(inDto);
+        // 댓글 추천/비추천
+        CommentVoteOut outDto = commentFacade.voteComment(inDto);
 
-		// response 변환
-		VoteCommentResponse response = modelMapper.map(outDto, VoteCommentResponse.class);
-		return ResponseEntity.ok(response);
-	}
+        // response 변환
+        VoteCommentResponse response = modelMapper.map(outDto, VoteCommentResponse.class);
+        return ResponseEntity.ok(response);
+    }
 }
